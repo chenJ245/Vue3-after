76485992c89432fcbd4ec7bcc1656f4e109dcd8f
chenJ245 <2452930499@qq.com>
2022-11-19 23:29:42 +0800

    feat(文章): 文章排名和详情和删除和路由分组

--- src/views/article-ranking/index.vue
+++ src/views/article-ranking/index.vue
@@ -1,9 +1,155 @@
 <template>
-  <div class="">文章排名</div>
+  <div class="article-ranking-container">
+    <el-card class="header">
+      <div class="dynamic-box">
+        <span class="title">{{ $t('msg.article.dynamicTitle') }}</span>
+        <el-checkbox-group v-model="selectDynamicLabel">
+          <el-checkbox
+            v-for="(item, index) in dynamicData"
+            :label="item.label"
+            :key="index"
+            >{{ item.label }}</el-checkbox
+          >
+        </el-checkbox-group>
+      </div>
+    </el-card>
+
+    <el-card>
+      <el-table ref="tableRef" :data="tableData" border>
+        <el-table-column
+          v-for="(item, index) in tableColumns"
+          :key="index"
+          :prop="item.prop"
+          :label="item.label"
+        >
+          <template v-if="item.prop === 'publicDate'" #default="{ row }">
+            {{ $filters.relativeTime(row.publicDate) }}
+          </template>
+          <template v-else-if="item.prop === 'action'" #default="{ row }">
+            <el-button type="primary" size="mini" @click="onShowClick(row)">{{
+              $t('msg.article.show')
+            }}</el-button>
+            <el-button type="danger" size="mini" @click="onRemoveClick(row)">{{
+              $t('msg.article.remove')
+            }}</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-pagination
+        class="pagination"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="page"
+        :page-sizes="[5, 10, 50, 100, 200]"
+        :page-size="size"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { ref, onActivated, onMounted } from 'vue'
+import { getArticleList, deleteArticle } from '@/api/article'
+import { watchSwitchLang } from '@/utils/i18n'
+import { dynamicData, selectDynamicLabel, tableColumns } from './dynamic'
+import { tableRef, initSortable } from './sortable'
+import { ElMessageBox, ElMessage } from 'element-plus'
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+
+// 数据相关
+const tableData = ref([])
+const total = ref(0)
+const page = ref(1)
+const size = ref(10)
+
+// 获取数据的方法
+const getListData = async () => {
+  const result = await getArticleList({
+    page: page.value,
+    size: size.value
+  })
+  tableData.value = result.list
+  total.value = result.total
+}
+getListData()
+watchSwitchLang(getListData)
+// 处理数据不重新加载的问题
+onActivated(getListData)
+
+// 初始化 sortable 表格数据初始化
+onMounted(() => {
+  initSortable(tableData, getListData)
+})
+
+// size 改变触发
+const handleSizeChange = (currentSize) => {
+  size.value = currentSize
+  getListData()
+}
+
+// page页码改变触发
+const handleCurrentChange = (currentPage) => {
+  page.value = currentPage
+  getListData()
+}
+
+// 点击查看
+const router = useRouter()
+const onShowClick = (row) => {
+  router.push(`/article/${row.id}`)
+}
+// 点击删除
+const i18n = useI18n()
+const onRemoveClick = (row) => {
+  ElMessageBox.confirm(
+    i18n.t('msg.article.dialogTitle1') +
+      row.title +
+      i18n.t('msg.article.dialogTitle2'),
+    {
+      type: 'warning'
+    }
+  ).then(async () => {
+    await deleteArticle(row.id)
+    ElMessage.success(i18n.t('msg.article.removeSuccess'))
+    // 重新渲染数据
+    getListData()
+  })
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.article-ranking-container {
+  .header {
+    margin-bottom: 20px;
+    .dynamic-box {
+      display: flex;
+      align-items: center;
+      .title {
+        margin-right: 20px;
+        font-size: 14px;
+        font-weight: bold;
+      }
+    }
+  }
+
+  ::v-deep .el-table__row {
+    cursor: pointer;
+  }
+
+  .pagination {
+    margin-top: 20px;
+    text-align: center;
+  }
+}
+
+::v-deep .sortable-ghost {
+  opacity: 0.6;
+  color: #fff !important;
+  background: #304156 !important;
+}
+</style>
